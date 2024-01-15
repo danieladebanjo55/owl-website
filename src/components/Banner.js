91073b2fedad4867d315c12ee72f67f6dc3bdd99
amit <amitexus2648@gmail.com>
2024-01-15 09:42:11 +0545

    completed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {useState } from "react";
 import { SlSocialTwitter } from "react-icons/sl";
 import { RiDiscordLine } from "react-icons/ri";
 import { LuSend } from "react-icons/lu";
@@ -11,61 +11,10 @@ function Banner() {
   // Set the draw date to your desired date and time
   const drawDate = new Date("Jan 15, 2024 18:00:00").getTime();
   
-
- 
-
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2000);
-
-  //   return () => clearTimeout(timeout);
-  // }, []);
-  // const [time, setTime] = useState(0);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setTime((prevTime) => prevTime + 1);
-  //   }, 1000); // Update every 1000 milliseconds (1 second)
-
-  //   // Clear the interval when the component unmounts
-  //   return () => clearInterval(interval);
-  // }, []);
-
-  // // Format the time to HH:MM:SS
-  // const formatTime = (seconds) => {
-  //   const hours = Math.floor(seconds / 3600);
-  //   const minutes = Math.floor((seconds % 3600) / 60);
-  //   const remainingSeconds = seconds % 60;
-
-  //   // Add leading zeros
-  //   const formattedHours = hours.toString().padStart(2, "0");
-  //   const formattedMinutes = minutes.toString().padStart(2, "0");
-  //   const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
-
-  //   return (
-  //     <div className="flex gap-x-2 items-center">
-  //       <div className="flex flex-col items-center">
-  //         <span className="bg-gray-600 mx-2">{formattedHours}:</span>
-  //         <span className="text-sm">Hours</span>
-  //       </div>
-  //       <div className="flex flex-col items-center">
-  //         <span className="bg-gray-600 mx-2">{formattedMinutes}:</span>
-  //         <span className="text-sm">Minutes</span>
-  //       </div>
-  //       <div className="flex flex-col items-center">
-  //         <span className="bg-gray-600 mx-2">{formattedSeconds}</span>
-  //         <span className="text-sm">Seconds</span>
-  //       </div>
-  //     </div>
-  //   );
-  // };
   return (
     <>
       <main className={`w-full lg:h-full md:px-5 lg:px-10`}>
-        {/* {loading && <LoadingScreen />} */}
-        {/* {!loading && <AnimatedBackground />} */}
-
+        {loading && <LoadingScreen />}
         <div className="flex w-full h-1/2 md:flex-row flex-col justify-between items-center lg:pt-10">
           <div className="lg:w-2/5 flex flex-col md:items-start items-center my-10">
             <h1 className="font-semibold  text-center md:text-start text-3xl lg:text-4xl text-white">
@@ -88,7 +37,6 @@ function Banner() {
             </div>
           </div>
         </div>
-
         <div className="flex md:flex-row flex-col items-center md:justify-between py-5 lg:pt-32 md:pt-28">
           <div className="text-2xl font-bold flex lg:gap-2 lg:flex-row flex-col lg:items-center items-center  justify-start text-[#F4CE14] mx-10 md:mx-0">
             <span className="underline text-3xl">Previous Winner </span>
@@ -101,7 +49,7 @@ function Banner() {
           <div className="flex items-center justify-center">
             <ul className="flex items-center space-x-15">
               <li className="">
-                <a href="#" className="">
+                <a href="/" className="">
                   <span className="relative z-10 flex justify-center items-center py-5">
                     <LuSend size={28} />
                   </span>
@@ -109,7 +57,7 @@ function Banner() {
                 </a>
               </li>
               <li className="">
-                <a href="#" className="">
+                <a href="/" className="">
                   <span className="relative z-10 flex justify-center items-center py-5">
                     <SlSocialTwitter size={28} />
                   </span>
@@ -117,7 +65,7 @@ function Banner() {
                 </a>
               </li>
               <li className="">
-                <a href="#" className="">
+                <a href="/" className="">
                   <span className="relative z-10 flex justify-center items-center py-5">
                     <RiDiscordLine size={28} />
                   </span>
--- src/components/Loadingscreen.js
+++ src/components/Loadingscreen.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import HashLoader from "react-spinners/HashLoader";
 
-const LoadingScreen = () => (
-  <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-80 flex justify-center items-center z-50">
-    <div className="border-4 border-yellow-500 border-solid rounded-full w-10 h-10 animate-spin"></div>
-    
-  </div>
-);
+const LoadingScreen = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-90 flex justify-center items-center z-50">
+          <HashLoader color="#FFFF00" loading={loading} size={50} />
+        </div>
+      ) : null}
+    </>
+  );
+};
 
 export default LoadingScreen;
+
+
