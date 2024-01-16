56a6821f1f26749500e055b2006d848e03f5355e
amit <amitexus2648@gmail.com>
2024-01-16 13:56:05 +0545

    added bg music

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
 import Banner from './components/Banner'
+import Playmusic from './components/Playmusic'
+
+
 
 function App() {
+  
   return (
     <>
+    <Playmusic/>
     <Header/>
     <Banner/>
     </>
---
+++ src/components/Playmusic.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import Sound from 'react-sound';
+import TheOtherSide from '../assets/music.mp3';
+
+function Playmusic() {
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const handlePlay = () => {
+    setIsPlaying(true);
+  };
+
+  const handleSongFinishedPlaying = () => {
+    // Handle song finished playing logic
+    setIsPlaying(true);
+  };
+
+  return (
+    <div>
+      {/* <button onClick={handlePlay}>Play Music</button> */}
+      <Sound
+        url={TheOtherSide}
+        playStatus={isPlaying ? Sound.status.PLAYING : ''}
+        playFromPosition={300}
+        onFinishedPlaying={handleSongFinishedPlaying}
+      />
+    </div>
+  );
+}
+
+export default Playmusic;
